6ac533cef97ab04da1d0824ebad71173228f009c
hyeongseokpark-29cm <devPaul@29cm.co.kr>
2023-07-14 15:08:09 +0900

    update SPM

--- Package.swift
+++ Package.swift
@@ -1,30 +1,33 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ImpressionKit",
-    platforms: [.iOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ImpressionKit",
             targets: ["ImpressionKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftHook", url: "https://github.com/623637646/SwiftHook.git", "3.3.0"..<"4.0.0")
+        .package(url: "https://github.com/623637646/SwiftHook.git", "3.3.0"..<"4.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        
-        // Source Code
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ImpressionKit",
-            dependencies: ["SwiftHook"],
-            path: "ImpressionKit",
-            exclude: ["ImpressionKit.h", "Info.plist"]),
+            dependencies: [
+              "SwiftHook",
+              "SwiftHookOCSources",
+            ],
+            path: "ImpressionKit"
+        ),
+        .testTarget(
+            name: "ImpressionKitTests",
+            dependencies: ["ImpressionKit"]),
     ]
 )
