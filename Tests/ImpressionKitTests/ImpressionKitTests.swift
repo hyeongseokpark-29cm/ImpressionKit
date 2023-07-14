03adac8c07d5fd90a12824cc43ecb8f610d9f111
hyeongseokpark-29cm <devPaul@29cm.co.kr>
2023-07-14 15:08:09 +0900

    update SPM

---
+++ Tests/ImpressionKitTests/ImpressionKitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import ImpressionKit
+
+final class ImpressionKitTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
